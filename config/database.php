70c610e562fca1ce2441740e2211bcedeb406df6
YUD96 <yudai000916@gmail.com>
2023-06-04 00:06:12 +0900

    初期設定

--- config/database.php
+++ config/database.php
@@ -22,7 +22,7 @@ return [
     | Database Connections
     |--------------------------------------------------------------------------
     |
-    | Here are each of the database connections setup for your application.
+    | Here are each of the database connections setup for your unix_socketapplication.
     | Of course, examples of configuring each database platform that is
     | supported by Laravel is shown below to make development simple.
     |
@@ -47,11 +47,12 @@ return [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'port' => env('DB_PORT', '8889'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
+            // 'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
